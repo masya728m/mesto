083ab8aabb9e3f8606ae3cf82f5ee78b42ec3022
marina <masya728m@gmail.com>
2021-05-01 13:37:13 +0300

    реализована инициализация карточек в скрипте

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,71 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelectorAll('.popup__field')[0];
-let jobInput = formElement.querySelectorAll('.popup__field')[1];
-let closeButton = document.querySelector('.popup__exit-button');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__description');
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const formElement = document.querySelector('.popup__container');
+const nameInput = formElement.querySelectorAll('.popup__field')[0];
+const jobInput = formElement.querySelectorAll('.popup__field')[1];
+const closeButton = document.querySelector('.popup__exit-button');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__description');
+const places = document.querySelector('.places');
+const addButton = document.querySelector('.profile__add-button');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: './images/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: './images/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: './images/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: './images/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: './images/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: './images/baikal.jpg'
+  }
+];
+
+function addCard(card) {
+  let placeImage = document.createElement('img');
+  placeImage.classList.add('places__image');
+  placeImage.setAttribute('src', card.link);
+  placeImage.setAttribute('alt', card.name);
+
+  let placeName = document.createElement('h2');
+  placeName.classList.add('places__name');
+  placeName.textContent = card.name;
+
+  let likeButton = document.createElement('button');
+  likeButton.classList.add('places__like-button');
+  likeButton.setAttribute('type', 'button');
+
+  let placeInfo = document.createElement('div');
+  placeInfo.classList.add('places__info');
+  placeInfo.append(placeName, likeButton);
+
+  let place = document.createElement('div');
+  place.classList.add('places__place');
+
+  place.append(placeImage, placeInfo);
+  places.append(place);
+}
+
+function fillCards(cards) {
+  cards.forEach(card => addCard(card));
+}
+
+fillCards(initialCards);
 
 
 function editButtonCLickHandler(event) {
